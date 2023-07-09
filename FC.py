6ccc43d41984b93486e2a805db2d1039596d9a81
avivwei96 <76816214+avivwei96@users.noreply.github.com>
2023-07-10 02:54:08 +0300

    uploading the mnist and finishing the backprop

--- FC.py
+++ FC.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+from tqdm import tqdm
 
 class fullyConnectedNN(object):
     def __init__(self, layers_sizes):
@@ -12,6 +14,7 @@ class fullyConnectedNN(object):
                          for layer_size_in, layer_size_out in zip(layers_sizes[:-1], layers_sizes[1:])]
         self.layer_out = None
         self.deltas = None
+        self.lr = 0.01
 
     def feed_forward(self, features, train=False):
         """
@@ -23,15 +26,15 @@ class fullyConnectedNN(object):
         # add one to the features for the bias
         res = features
         if train:
-            ones_column = np.ones((res.shape[0], 1))
-            features = np.concatenate((features, ones_column), axis=1)
-            self.layer_out = [features]
+            self.layer_out = []
         for weight_mat in self._weights:
-            ones_column = np.ones((res.shape[0], 1))
+            ones_column = np.ones((len(res), 1))
             res = np.concatenate((res, ones_column), axis=1)
             if train:
-                self.layer_out.append(res @ weight_mat)
+                self.layer_out.append(res)
             res = np.tanh(res @ weight_mat)
+        if train:
+            self.layer_out.append(res)
         return res
 
     def back_prop(self, features, labels):
@@ -41,7 +44,7 @@ class fullyConnectedNN(object):
         :param labels: (np array) The corresponding labels for the input features.
         """
         #init deltas and computing der
-        self.deltas = [np.zeros((features.shape[0], layer_size), dtype=float) for layer_size in self.layers_sizes[1:]]
+        self.deltas = [np.zeros((len(features), layer_size), dtype=float) for layer_size in self.layers_sizes[1:]]
         net_res = self.softmax(self.feed_forward(features, train=True))
         error_der = self.cross_entropy_derivative(net_res, labels)
         # compute the first delta
@@ -50,18 +53,52 @@ class fullyConnectedNN(object):
         # implementing the back prop rule
         for layer_i in range(2, self.n_layers):
             sum_of_Del_W = self._weights[-layer_i + 1][:-1] @ self.deltas[-layer_i + 1].T
-            print(sum_of_Del_W.T)
-            print((1 - np.tanh(self.layer_out[-layer_i])**2))
-            self.deltas[-layer_i] = (1 - np.tanh(self.layer_out[-layer_i])**2) * sum_of_Del_W.T
+            res = 1 - np.tanh(self.layer_out[-layer_i])**2
+            res = np.delete(res, -1, axis=1)
+            self.deltas[-layer_i] = res * sum_of_Del_W.T
+
 
     def update_weights(self):
         for layer_i in range(0, self.n_layers - 1):
-            summed_delta = np.sum(self.deltas[layer_i], axis=1)
+            der = self.layer_out[layer_i][:, np.newaxis, :] * self.deltas[layer_i][:, :, np.newaxis]
+            der = np.sum(der, axis=0)
+            self._weights[layer_i] -= self.lr*der.T
 
+    def predict_proba(self, features):
+        return self.softmax(self.feed_forward(features))
+
+    def predict(self, features):
+        proba_vec = self.predict_proba(features)
+        return np.argmax(proba_vec, axis=1)
+
+    def train(self, features, labels, epochs=100, lr=0.01, batch_size=32):
+        self.lr = lr
+        for epoch in range(epochs):
+            # Shuffle the training data
+            combined_data = list(zip(features, labels))
+            random.shuffle(combined_data)
+            features_shuffled, labels_shuffled = zip(*combined_data)
+
+            # Create mini-batches
+            mini_batches = [
+                (features_shuffled[k:k + batch_size], labels_shuffled[k:k + batch_size])
+                for k in range(0, len(features), batch_size)
+            ]
+
+            # Iterate over mini-batches and update model parameters
+            for mini_batch_features, mini_batch_labels in tqdm(mini_batches):
+                self.back_prop(mini_batch_features, mini_batch_labels)
+                self.update_weights()
+
+    def score(self, features, labels):
+        res = self.predict(features)
+        correct_predictions = np.sum(labels == res)
+        return correct_predictions / len(labels)
 
     @staticmethod
     def cross_entropy(pred_prob, true_label):
         return -sum(np.log(pred_prob) @ true_label)
+
     @staticmethod
     def softmax(values):
         exponent_val = np.exp(values)
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from FC import fullyConnectedNN as fc
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-net = fc([784, 200, 150, 10])
-random_list = np.random.randn(2,784)
-labels = [1, 0, 0, 1]
-net.back_prop(random_list,labels = [0, 0, 0, 0, 0, 0, 1, 0, 0, 1])
+X = np.load('MNIST-data.npy')
+y = np.load("MNIST-lables.npy")
+
+labels = np.zeros((len(y), 10))
+labels[np.arange(len(y)), y] = 1
+features = X.reshape((X.shape[0], -1))
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+X_vladition, X_test, y_vladition, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+# Convert the tuples to arrays
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+lr = [0.01]
+net = fc([784, 100, 10])
+net.train(X_train, y_train, epochs=10)
+print(net.score(X_test, np.argmax(y_test, axis=1)))
